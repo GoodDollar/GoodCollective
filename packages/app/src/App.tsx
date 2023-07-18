62fe79a708f8106e5230680da895e4539c73d23f
sirpy <hadarr@gmail.com>
2023-07-18 17:03:01 +0200

    add: demo

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -5,12 +5,14 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { PropsWithChildren } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { Linking, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { ConnectWallet } from './components/ConnectWallet';
 import { useEthers } from '@usedapp/core';
-import { useNativeBalance } from '@gooddollar/web3sdk-v2';
+import { useNativeBalance, ClaimSDK } from '@gooddollar/web3sdk-v2';
+import * as ethers from 'ethers';
+import { Button } from 'native-base';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -25,26 +27,65 @@ function Section({ children, title }: SectionProps): JSX.Element {
   );
 }
 
+let sdk = new ClaimSDK(new ethers.providers.JsonRpcProvider('https://forno.celo.org'), 'development-celo');
 function App(): JSX.Element {
+  const [status, setStatus] = useState();
+  const [faucet, setFaucet] = useState();
+  const [claim, setClaim] = useState();
+
   const isDarkMode = useColorScheme() === 'dark';
-  const { account } = useEthers();
+  const { account, library } = useEthers();
   const backgroundStyle = {};
   const balance = useNativeBalance();
+
+  const startFV = async () => {
+    const fvlink = await sdk.generateFVLink('Hadar', window.location.href, false);
+    Linking.openURL(fvlink);
+  };
+
+  const startFaucet = async () => {
+    await sdk.getContract('Faucet').topWallet(account);
+  };
+
+  const startClaim = async () => {
+    await sdk.claim();
+  };
+
+  useEffect(() => {
+    if (account) {
+      sdk = new ClaimSDK(library as any, 'development-celo');
+      console.log(sdk.contracts);
+      sdk.isAddressVerified(account).then((_) => setStatus(_));
+      sdk.checkEntitlement().then((_) => setClaim(ethers.utils.formatEther(_)));
+      console.log(sdk.getContract('Faucet').address);
+
+      sdk
+        .getContract('Faucet')
+        .canTop(account)
+        .then((_) => setFaucet(_));
+    }
+  }, [account, library]);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
         <View>
-          <Section title="Step One">
-            {balance}
-            Edit <Text style={styles.highlight}>App.tsx</Text> xxxto change this screen and then come back to see your
-            edits.
-          </Section>
-          <Section title="See Your Changes" />
-          <Section title="Debug" />
-          <Section title="Learn More">Read the docs to discover what to do next:</Section>
           <ConnectWallet />
+
           <Section title="Wallet">{account}</Section>
+          <Section title="Balance">{balance?.toString()}</Section>
+          <Section title="Whitelisted">{String(status)}</Section>
+          <Section title="Get Whitelisted">
+            <Button onPress={startFV}>Start Verification</Button>
+          </Section>
+          <Section title="Can top gas">{String(faucet)}</Section>
+          <Section title="Get gas">
+            <Button onPress={startFaucet}>Top Wallet</Button>
+          </Section>
+          <Section title="Claim">
+            <Button onPress={startClaim}>{Number(claim).toFixed(2)} G$</Button>
+          </Section>
         </View>
       </ScrollView>
     </SafeAreaView>
