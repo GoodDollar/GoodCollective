4d51b762fb6994671c5e8fbcb479af4f94b2c3b5
sirpy <hadarr@gmail.com>
2023-08-07 15:02:32 +0300

    fix: web3button

--- packages/app/src/components/ConnectWallet.native.tsx
+++ packages/app/src/components/ConnectWallet.native.tsx
@@ -1,2 +1,10 @@
-import { Web3Button } from '@web3modal/react-native';
-export const ConnectWallet = Web3Button;
+import React from 'react';
+import { Button } from 'native-base';
+import { useWalletConnectModal } from '@walletconnect/modal-react-native';
+
+export const ConnectWallet = () => {
+  const wcModal = useWalletConnectModal();
+
+  const walletConnect = () => wcModal.open();
+  return <Button onPress={walletConnect}>Connect</Button>;
+};
