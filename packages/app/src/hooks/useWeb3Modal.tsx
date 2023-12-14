eb83116a3072ed6b35290a5dede2b0c4a2de0ea7
LewisB <lewis@gooddollar.org>
2023-12-14 14:47:32 +0700

    fix: forgot to comment out web3modal

--- packages/app/src/hooks/useWeb3Modal.tsx
+++ packages/app/src/hooks/useWeb3Modal.tsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
-import { EthereumProvider } from '@walletconnect/ethereum-provider';
-import { IEthereumProvider } from '@walletconnect/ethereum-provider/dist/types/EthereumProvider';
-const projectId = '2d923a8a66e396445ba00cd8b882450b';
+// import { useState, useEffect } from 'react';
+// import { EthereumProvider } from '@walletconnect/ethereum-provider';
+// import { IEthereumProvider } from '@walletconnect/ethereum-provider/dist/types/EthereumProvider';
+// const projectId = '2d923a8a66e396445ba00cd8b882450b';
 
-export const useWeb3Modal = () => {
-  const [provider, setProvider] = useState<IEthereumProvider>();
+// export const useWeb3Modal = () => {
+//   const [provider, setProvider] = useState<IEthereumProvider>();
 
-  useEffect(() => {
-    EthereumProvider.init({
-      projectId, // REQUIRED your projectId
-      chains: [122, 42220], // REQUIRED chain ids
-      showQrModal: true, // REQUIRED set to "true" to use @walletconnect/modal
-    }).then((_) => setProvider(_));
-  }, []);
+//   useEffect(() => {
+//     EthereumProvider.init({
+//       projectId, // REQUIRED your projectId
+//       chains: [122, 42220], // REQUIRED chain ids
+//       showQrModal: true, // REQUIRED set to "true" to use @walletconnect/modal
+//     }).then((_) => setProvider(_));
+//   }, []);
 
-  return provider;
-};
+//   return provider;
+// };
