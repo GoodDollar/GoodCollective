31ff9aebe44e17528c7be190396d27d44a46c5d6
kris <kris@dorg.tech>
2023-12-30 12:12:44 +0500

    added index to hooks/useContractCalls

--- packages/app/src/hooks/index.ts
+++ packages/app/src/hooks/index.ts
@@ -2,6 +2,6 @@ export * from './useCollectiveById';
 export * from './useDonorById';
 export * from './useIsDonorOfCollective';
 export * from './useStewardById';
-export * from './useContractCalls/useContractCalls';
+export * from './useContractCalls';
 export * from './useGetTokenPrice';
 export * from './wagmiF';
