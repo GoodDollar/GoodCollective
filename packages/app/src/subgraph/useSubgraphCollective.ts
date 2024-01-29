a841c38771acb09dceeca32933fb8787cb88e0cc
kris <kris@dorg.tech>
2024-01-29 16:20:00 +0500

    fixed graphql query so ensure social links are obtained; made sure social links open in new tab

--- packages/app/src/subgraph/useSubgraphCollective.ts
+++ packages/app/src/subgraph/useSubgraphCollective.ts
@@ -2,9 +2,9 @@ import { gql } from '@apollo/client';
 import { CollectivesSubgraphResponse, useSubgraphData } from './useSubgraphData';
 import { SubgraphCollective } from './subgraphModels';
 
-export const collective = gql`
-  query COLLECTIVE($id: String) {
-    collectives(where: { id: $id }) {
+export const collectivesById = gql`
+  query COLLECTIVES_BY_ID($ids: [String!]) {
+    collectives(where: { id_in: $ids }) {
       id
       ipfs {
         id
@@ -48,60 +48,6 @@ export const collective = gql`
     }
   }
 `;
-
-export const collectivesById = gql`
-  query COLLECTIVES_BY_ID($ids: [String!]) {
-    collectives(where: { id_in: $ids }) {
-      id
-      ipfs {
-        id
-        name
-        description
-        headerImage
-      }
-      stewards {
-        id
-        steward {
-          id
-        }
-        collective {
-          id
-        }
-        actions
-        totalEarned
-      }
-      donors {
-        id
-        donor {
-          id
-        }
-        collective {
-          id
-        }
-        contribution
-        timestamp
-        flowRate
-      }
-      timestamp
-      paymentsMade
-      totalDonations
-      totalRewards
-    }
-  }
-`;
-
-export function useSubgraphCollective(id: string): SubgraphCollective | undefined {
-  const response = useSubgraphData(collective, {
-    variables: {
-      id: id,
-    },
-  });
-  const data = (response as CollectivesSubgraphResponse).collectives;
-  if (!data || data.length === 0) {
-    return undefined;
-  }
-  return data[0];
-}
 
 export function useSubgraphCollectivesById(ids: string[]): SubgraphCollective[] | undefined {
   const response = useSubgraphData(collectivesById, {
