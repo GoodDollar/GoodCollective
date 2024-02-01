1f03125ba2410a54075f308957d1f897b57f2803
Kris Bitney <kris@dorg.tech>
2024-02-02 00:54:56 +0500

    reduced reads of celo token list json file (#148)
    Co-authored-by: LewisB <lewis@gooddollar.org>

--- packages/app/src/hooks/useTokenList.ts
+++ packages/app/src/hooks/useTokenList.ts
@@ -1,22 +1,25 @@
-import { useMemo } from 'react';
 import { Token } from '@uniswap/sdk-core';
 import CeloTokenList from '../models/CeloTokenList.json';
 import { SupportedNetwork } from '../models/constants';
 
+const populatedTokenList: Record<string, Token> = populateTokenList();
+
+function populateTokenList(): Record<string, Token> {
+  const tokenList: Record<string, Token> = {};
+  const sortedList = CeloTokenList.tokens.sort((a, b) => a.symbol.localeCompare(b.symbol));
+  sortedList.forEach((token) => {
+    if (token.chainId !== SupportedNetwork.CELO) {
+      return;
+    }
+    tokenList[token.symbol] = new Token(token.chainId, token.address, token.decimals, token.symbol);
+  });
+  return tokenList;
+}
+
 export function useToken(symbol: string): Token {
-  return useTokenList()[symbol];
+  return populatedTokenList[symbol];
 }
 
 export function useTokenList(): Record<string, Token> {
-  return useMemo(() => {
-    const tokenList: Record<string, Token> = {};
-    const sortedList = CeloTokenList.tokens.sort((a, b) => a.symbol.localeCompare(b.symbol));
-    sortedList.forEach((token) => {
-      if (token.chainId !== SupportedNetwork.CELO) {
-        return;
-      }
-      tokenList[token.symbol] = new Token(token.chainId, token.address, token.decimals, token.symbol);
-    });
-    return tokenList;
-  }, []);
+  return populatedTokenList;
 }
