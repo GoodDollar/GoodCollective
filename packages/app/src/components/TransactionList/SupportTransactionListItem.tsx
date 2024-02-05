67d796dbea5818f9a5c4a8793d5a02f50a589b04
sirpy <hadar@gooddollar.org>
2024-02-05 13:09:44 +0200

    add: use monthly flow rate for display

--- packages/app/src/components/TransactionList/SupportTransactionListItem.tsx
+++ packages/app/src/components/TransactionList/SupportTransactionListItem.tsx
@@ -3,29 +3,27 @@ import { formatAddress } from '../../lib/formatAddress';
 import { useEnsName } from 'wagmi';
 import Decimal from 'decimal.js';
 import TransactionListItem from './TransactionListItem';
-import { useFlowingBalance } from '../../hooks/useFlowingBalance';
 import { useFetchFullName } from '../../hooks/useFetchFullName';
+import { calculateGoodDollarAmounts } from '../../lib/calculateGoodDollarAmounts';
 
 interface SupportTransactionListItemProps {
   transaction: SupportTx;
 }
 
+const MONTH_SECONDS = 2592000;
 export function SupportTransactionListItem({ transaction }: SupportTransactionListItemProps) {
-  const { hash, networkFee, timestamp, donor } = transaction;
+  const { hash, networkFee, donor } = transaction;
 
   const userAddress = donor as `0x${string}`;
   const { data: ensName } = useEnsName({ address: userAddress, chainId: 1 });
   const userFullName = useFetchFullName(userAddress);
   const userIdentifier = userFullName ?? ensName ?? formatAddress(userAddress);
 
-  const { formatted: formattedAmount } = useFlowingBalance(
-    transaction.contribution,
-    timestamp,
-    transaction.flowRate,
-    undefined
-  );
+  const monthlyFlowRate =
+    transaction.isFlowUpdate &&
+    calculateGoodDollarAmounts(String(BigInt(transaction.flowRate) * BigInt(MONTH_SECONDS))).formatted;
   const amount = transaction.isFlowUpdate
-    ? formattedAmount
+    ? `${monthlyFlowRate} / Month`
     : new Decimal(transaction.contribution).minus(transaction.previousContribution).toString();
 
   return (
