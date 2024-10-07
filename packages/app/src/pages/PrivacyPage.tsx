74e6d795653efdc50091cd9a64d80a43a51233e4
sirpy <hadar@gooddollar.org>
2024-10-07 16:57:23 +0300

    add: Link to T&Cs and Privacy Policy from Menu #175

---
+++ packages/app/src/pages/PrivacyPage.tsx
@@ -0,0 +1,7 @@
+import { WebviewPage } from './WebviewPage';
+
+const PrivacyPage = () => {
+  return <WebviewPage uri={'https://www.gooddollar.org/privacy-policy#'} />;
+};
+
+export default PrivacyPage;
---
+++ packages/app/src/routes/routing.ts
@@ -0,0 +1,1 @@
+export { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
