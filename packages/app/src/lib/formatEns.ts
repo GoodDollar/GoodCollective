e2378d7938d4eea48b7e6c8504741dc841c5290e
Lewis B <lewis@ikigaistudios.eu>
2025-02-05 17:38:56 +0900

    Apply suggestions from code review

--- packages/app/src/lib/formatEns.ts
+++ packages/app/src/lib/formatEns.ts
@@ -1,7 +1,6 @@
-export const formatEns = (ensName: string) => {
+export const formatEns = (ensName: string, targetLength = 12) => {
   if (!ensName) return '';
   const [name, extension] = ensName.split('.');
-  const targetLength = 12;
   const extensionFull = extension ? `.${extension}` : '';
   const availableLength = targetLength - extensionFull.length;
   const truncatedName = name.slice(0, availableLength / 2) + '...' + name.slice(-(availableLength / 2 - 3));
