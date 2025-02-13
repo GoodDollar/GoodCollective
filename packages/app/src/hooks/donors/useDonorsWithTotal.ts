0fb663acbe71eabd9df334fdd8eaa9a596b71cdd
Alison Hawk <alihawk360@gmail.com>
2025-02-14 03:28:28 +0600

    Optimize donors list sorting with stable sorting and memoization

--- packages/app/src/hooks/donors/useDonorsWithTotal.ts
+++ packages/app/src/hooks/donors/useDonorsWithTotal.ts
@@ -3,7 +3,10 @@ import { DonorCollective } from '../../models/models';
 import { useDonorWithTotal } from './useDonorWithTotal';
 
 export function useDonorsWithTotal(donors: DonorCollective[]) {
-  const donorsWithTotal = donors.map(useDonorWithTotal);
+  const stableDonors = useMemo(() => {
+    return [...donors].sort((a, b) => (a.donor > b.donor ? 1 : -1));
+  }, [donors]);
+  const donorsWithTotal = stableDonors.map(useDonorWithTotal);
 
   return useMemo(
     () => donorsWithTotal.sort((a, b) => (b.totalDonations > a.totalDonations ? 1 : -1)),
