7b375ba3fa73eabb0cda303195d1a833f5738c44
Alison Hawk <alihawk360@gmail.com>
2025-02-13 12:13:26 -0800

    Refactor donors list sorting with total donations calculation

--- packages/app/src/components/DonorsList/DonorsList.tsx
+++ packages/app/src/components/DonorsList/DonorsList.tsx
@@ -2,8 +2,8 @@ import { Platform, StyleSheet, View } from 'react-native';
 import { DonorCollective } from '../../models/models';
 import { DonorsListItem } from './DonorsListItem';
 import { useMemo } from 'react';
-import Decimal from 'decimal.js';
 import { useFetchFullNames } from '../../hooks/useFetchFullName';
+import { useDonorsWithTotal } from '../../hooks/useDonorsWithTotal';
 
 interface DonorsListProps {
   donors: DonorCollective[];
@@ -13,13 +13,7 @@ interface DonorsListProps {
 // TODO: DonorsList and StewardsList will not look correct on Desktop when there are more than 5 donors. They will in one column, but they should be in three columns.
 
 function DonorsList({ donors, listStyle }: DonorsListProps) {
-  const sortedDonors: DonorCollective[] = useMemo(() => {
-    return donors.sort((a, b) => {
-      const aDecimal = new Decimal(a.contribution ?? 0);
-      const bDecimal = new Decimal(b.contribution ?? 0);
-      return bDecimal.cmp(aDecimal);
-    });
-  }, [donors]);
+  const sortedDonors = useDonorsWithTotal(donors);
 
   const userAddresses = useMemo(() => {
     return sortedDonors.map((donor) => donor.donor as `0x${string}`);
