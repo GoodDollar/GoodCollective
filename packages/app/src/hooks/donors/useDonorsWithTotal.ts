cbfb99aa951e0926a8d0e75a2f35a9ac2b8d73e0
Alison Hawk <alihawk360@gmail.com>
2025-02-18 11:22:23 +0600

    Replace sortBy with orderBy for more explicit donor sorting

--- packages/app/src/hooks/donors/useDonorsWithTotal.ts
+++ packages/app/src/hooks/donors/useDonorsWithTotal.ts
@@ -1,14 +1,14 @@
 import { useMemo } from 'react';
 import { DonorCollective } from '../../models/models';
 import { useDonorWithTotal } from './useDonorWithTotal';
-import { sortBy } from 'lodash';
+import { orderBy } from 'lodash';
 
 export function useDonorsWithTotal(donors: DonorCollective[]) {
   // Sort by donor address for stable hook ordering
-  const stableDonors = useMemo(() => sortBy(donors, 'donor'), [donors]);
+  const stableDonors = useMemo(() => orderBy(donors, 'donor'), [donors]);
 
   const donorsWithTotal = stableDonors.map(useDonorWithTotal);
 
   // Sort by total donations in descending order
-  return useMemo(() => sortBy(donorsWithTotal, (d) => d.totalDonations).reverse(), [donorsWithTotal]);
+  return useMemo(() => orderBy(donorsWithTotal, 'totalDonations', 'desc'), [donorsWithTotal]);
 }
