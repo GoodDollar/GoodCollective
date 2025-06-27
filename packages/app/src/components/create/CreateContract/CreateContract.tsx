0aca4c005ff522a1d3bbd77f28caf7c5b8ab3428
Peter Kompasz <kompaszbence@gmail.com>
2025-06-27 22:39:00 +0300

    fix: style

--- packages/app/src/components/create/CreateContract/CreateContract.tsx
+++ packages/app/src/components/create/CreateContract/CreateContract.tsx
@@ -1,8 +1,9 @@
-import { Button, HStack, VStack } from 'native-base';
+import { Box, Button, HStack, Progress, VStack } from 'native-base';
 import GetStarted from './1GetStarted';
 import ProjectDetails from './2ProjectDetails';
 import PoolConfiguration from './3PoolConfiguration';
 import ReviewLaunch from './4ReviewLaunch';
+import ActionButton from '../../ActionButton';
 
 const CreateContract = ({
   step,
@@ -15,17 +16,22 @@ const CreateContract = ({
 }) => {
   return (
     <VStack>
+      <Box backgroundColor="blueGray.400">
+        <Progress colorScheme="secondary" value={5 + 30 * (step - 2)} />
+        <HStack style={{ width: '100%', justifyContent: 'space-between' }}>
+          <p style={{ color: step === 2 ? 'white' : 'grey' }}>Get Started(1/4)</p>
+          <p style={{ color: step === 3 ? 'white' : 'grey' }}>Create Pool(2/4)</p>
+          <p style={{ color: step === 4 ? 'white' : 'grey' }}>Pool Configuraiton(3/4)</p>
+          <p style={{ color: step === 5 ? 'white' : 'grey' }}>Review & Launch(4/4)</p>
+        </HStack>
+      </Box>
       {step === 2 && <GetStarted />}
       {step === 3 && <ProjectDetails />}
       {step === 4 && <PoolConfiguration />}
       {step === 5 && <ReviewLaunch />}
       <HStack w="full" justifyContent="space-between">
-        <Button size="sm" variant="outline" colorScheme="secondary" onPress={() => onStepBackward()}>
-          Back
-        </Button>
-        <Button size="sm" colorScheme="secondary" onPress={() => onStepForward()}>
-          Next
-        </Button>
+        <ActionButton onPress={() => onStepBackward()} text={'Back'} bg="white" textColor="black" />
+        <ActionButton onPress={() => onStepForward()} text={'Next'} bg="goodPurple.400" textColor="white" />
       </HStack>
     </VStack>
   );
