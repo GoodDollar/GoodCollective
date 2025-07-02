bf90e3119881711b0ff3b310e69413991eb5a9e8
Peter Kompasz <kompaszbence@gmail.com>
2025-07-02 23:29:08 +0300

    chore: simplify

--- packages/app/src/components/create/CreateGoodCollective.tsx
+++ packages/app/src/components/create/CreateGoodCollective.tsx
@@ -1,59 +1,19 @@
-import { useState } from 'react';
 import Welcome from './Welcome';
 import SelectType from './SelectCollectiveType';
 import CreateContract from './CreateContract/CreateContract';
 import Success from './Success';
-
-export type Form = {
-  projectName?: string;
-  projectDescription?: string;
-  tagline?: string;
-  logo?: string;
-  coverPhoto?: string;
-  adminWalletAddress?: string;
-  additionalInfo?: string;
-  poolManagerFeeType?: 'default' | 'custom';
-  claimFrequency?: 'day' | 'week' | '14days' | '30days' | 'custom';
-  joinStatus?: 'closed' | 'open';
-  website?: string;
-  twitter?: string;
-  telegram?: string;
-  discord?: string;
-  facebook?: string;
-};
+import { useCreatePool } from '../../hooks/useCreatePool';
 
 const CreateGoodCollective = () => {
-  const [step, setStep] = useState(0);
-
-  // We store the form in the top-most component so we can jump back and forth between steps
-  const [form, setForm] = useState<Form>({});
-
-  const submitPartial = (partialForm: Form) => {
-    console.log(partialForm);
-    setForm({
-      ...form,
-      ...partialForm,
-    });
-  };
+  const { step } = useCreatePool();
 
   return (
-    <>
-      {Object.keys(form).length}
-      <div>
-        {step === 0 && <Welcome onStepForward={async () => setStep(step + 1)} />}
-        {step === 1 && <SelectType onStepForward={async () => setStep(step + 1)} onPartialSubmit={submitPartial} />}
-        {step >= 2 && step <= 5 && (
-          <CreateContract
-            form={form}
-            onPartialSubmit={submitPartial}
-            step={step}
-            onStepBackward={async () => setStep(step - 1)}
-            onStepForward={async () => setStep(step + 1)}
-          />
-        )}
-        {step === 6 && <Success />}
-      </div>
-    </>
+    <div>
+      {step === 0 && <Welcome />}
+      {step === 1 && <SelectType />}
+      {step >= 2 && step <= 5 && <CreateContract />}
+      {step === 6 && <Success />}
+    </div>
   );
 };
 
