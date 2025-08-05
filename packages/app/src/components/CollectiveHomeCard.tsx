fb2befbb1ae520109a1eee97ce45d1f4b76ecc59
LewisB <lewis@gooddollar.org>
2025-08-06 00:00:17 +0700

    fix: limit height of collective card title

--- packages/app/src/components/CollectiveHomeCard.tsx
+++ packages/app/src/components/CollectiveHomeCard.tsx
@@ -56,7 +56,7 @@ const styles = StyleSheet.create({
     marginTop: 0,
     ...InterSemiBold,
     lineHeight: 25,
-    maxHeight: 25,
+    // maxHeight: 25,
     overflow: 'hidden',
   },
   cardDescription: {
