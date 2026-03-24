6c99e06f2917940e02ef917d1b0a05a34c2d0063
sirpy <hadar@gooddollar.org>
2026-03-24 14:35:25 +0200

    add: verify dev contracts. upgrade hardhat verify

---
+++ packages/contracts/test/Trust/Trust.superapp.test.ts
@@ -0,0 +1,258 @@
+import { deploySuperGoodDollar } from '@gooddollar/goodprotocol';
+import { loadFixture, mine } from '@nomicfoundation/hardhat-network-helpers';
+import { deployTestFramework } from '@superfluid-finance/ethereum-contracts/dev-scripts/deploy-test-framework';
+import { Framework } from '@superfluid-finance/sdk-core';
+
+import { expect } from 'chai';
+import { DirectPaymentsPool, ProvableNFT, UBIPool, UBIPoolFactory } from 'typechain-types';
+import { ethers, upgrades, network } from 'hardhat';
+import ERC20ABI from '@openzeppelin/contracts/build/contracts/ERC20PresetMinterPauser.json';
+
+type SignerWithAddress = Awaited<ReturnType<typeof ethers.getSigner>>;
+
+describe.skip('Trust Superapp', () => {
+  let signer: SignerWithAddress;
+  let signers: SignerWithAddress[];
+  let gdframework: Awaited<ReturnType<typeof deploySuperGoodDollar>>;
+  let sf: Framework;
+  const baseFlowRate = ethers.BigNumber.from(400e9).toString(); //enough to pass min flow rate of 386e9
+  let factory: UBIPoolFactory
+  let pool: UBIPool
+
+  before(async () => {
+    const { frameworkDeployer } = await deployTestFramework();
+    sf = await frameworkDeployer.getFramework();
+    // initialize framework
+    const opts = {
+      chainId: network.config.chainId || 31337,
+      provider: ethers.provider,
+      resolverAddress: sfFramework.resolver,
+      protocolReleaseVersion: 'test',
+    };
+    sf = await Framework.create(opts);
+
+    signers = await ethers.getSigners();
+    gdframework = await deploySuperGoodDollar(signers[0], sf, [
+      ethers.constants.AddressZero,
+      ethers.constants.AddressZero,
+    ]);
+    signer = signers[0];
+  });
+
+  const fixture = async () => {
+    const f = await ethers.getContractFactory('TrustPoolFactory');
+
+    const dpimpl = await ethers.deployContract('TrustPool', [sf['host']], {
+      libraries: { HelperLibrary: helper.address },
+    });
+
+
+    factory = (await upgrades.deployProxy(f, [signer.address, dpimpl.address, nft.address, signers[1].address, 1000], {
+      unsafeAllowLinkedLibraries: true,
+      kind: 'uups',
+    })) as UBIPoolFactory;
+
+    // return {pool, nft};
+  };
+
+  beforeEach(async function () {
+    await loadFixture(fixture);
+  });
+
+  it('should not be able to stream less than MIN_FLOW_RATE', async () => {
+    await gdframework.GoodDollar.mint(signer.address, ethers.constants.WeiPerEther);
+    const st = await sf.loadSuperToken(gdframework.GoodDollar.address);
+    const beforeBalance = await gdframework.GoodDollar.balanceOf(pool.address);
+    await expect(
+      st
+        .createFlow({
+          receiver: pool.address,
+          sender: signer.address,
+          flowRate: ((await pool.MIN_FLOW_RATE()).toNumber() - 1).toString(),
+        })
+        .exec(signer)
+    ).revertedWithCustomError(pool, 'MIN_FLOWRATE');
+  });
+
+  it('should be able to stream G$s', async () => {
+    await gdframework.GoodDollar.mint(signer.address, ethers.constants.WeiPerEther);
+    const st = await sf.loadSuperToken(gdframework.GoodDollar.address);
+    const beforeBalance = await gdframework.GoodDollar.balanceOf(pool.address);
+    const result = await st
+      .createFlow({ receiver: pool.address, sender: signer.address, flowRate: baseFlowRate })
+      .exec(signer);
+    await mine(2, { interval: 5 });
+
+    expect(await gdframework.GoodDollar.balanceOf(pool.address)).gte(beforeBalance.add(Number(baseFlowRate) * 5));
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution).equal(0);
+    expect(supporter.lastUpdated).gt(0);
+    expect(supporter.flowRate).equal(Number(baseFlowRate));
+  });
+
+  it('should be able to stop streaming G$s', async () => {
+    await gdframework.GoodDollar.mint(signer.address, ethers.constants.WeiPerEther);
+    const st = await sf.loadSuperToken(gdframework.GoodDollar.address);
+    const result = await st
+      .createFlow({ receiver: pool.address, sender: signer.address, flowRate: baseFlowRate })
+      .exec(signer);
+    await mine(2, { interval: 5 });
+
+    expect(await gdframework.GoodDollar.balanceOf(pool.address)).gte(Number(baseFlowRate) * 5);
+    await st.deleteFlow({ receiver: pool.address, sender: signer.address }).exec(signer);
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution).gt(Number(baseFlowRate) * 5);
+    expect(supporter.lastUpdated).gt(0);
+    expect(supporter.flowRate).equal(0);
+  });
+
+  it('should be able to update streaming G$s', async () => {
+    await gdframework.GoodDollar.mint(signer.address, ethers.constants.WeiPerEther.mul(10000000));
+    const st = await sf.loadSuperToken(gdframework.GoodDollar.address);
+    const result = await st
+      .createFlow({ receiver: pool.address, sender: signer.address, flowRate: baseFlowRate })
+      .exec(signer);
+    await mine(2, { interval: 5 });
+
+    expect(await gdframework.GoodDollar.balanceOf(pool.address)).gte(Number(baseFlowRate) * 5);
+    const before = await pool.supporters(signer.address);
+
+    await st
+      .updateFlow({
+        receiver: pool.address,
+        sender: signer.address,
+        flowRate: ethers.constants.WeiPerEther.toString(),
+      })
+      .exec(signer);
+    await mine(2, { interval: 5 });
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution)
+      .gt(Number(baseFlowRate) * 5)
+      .gt(before.contribution);
+    expect(supporter.lastUpdated).gt(before.lastUpdated);
+    expect(supporter.flowRate).equal(ethers.constants.WeiPerEther.toString());
+  });
+
+  it('should be able to stream G$s with batch', async () => {
+    await gdframework.GoodDollar.mint(signer.address, ethers.constants.WeiPerEther);
+    const st = await sf.loadSuperToken(gdframework.GoodDollar.address);
+    const flowAction = st.createFlow({
+      receiver: pool.address,
+      sender: signer.address,
+      flowRate: baseFlowRate,
+    });
+
+    const bc = sf.batchCall([flowAction]);
+    await bc.exec(signer);
+    await mine(2, { interval: 5 });
+
+    expect(await gdframework.GoodDollar.balanceOf(pool.address)).gte(Number(baseFlowRate) * 5);
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution).equal(0);
+    expect(supporter.lastUpdated).gt(0);
+    expect(supporter.flowRate).equal(Number(baseFlowRate));
+  });
+
+  it('should be able to support with single contribution using transferAndCall', async () => {
+    //mint to the swaprouter so it has G$s to send in exchange
+    await gdframework.GoodDollar.mint(signer.address, ethers.constants.WeiPerEther);
+    const st = gdframework.GoodDollar;
+    const transferAction = await st.transferAndCall(pool.address, 1000, '0x');
+    expect(await gdframework.GoodDollar.balanceOf(pool.address)).eq(1000);
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution).equal(1000);
+    expect(supporter.lastUpdated).eq(0); //no update on single donation
+    expect(supporter.flowRate).equal(0);
+  });
+
+  it('should be able to support with single contribution by calling support', async () => {
+    //mint to the swaprouter so it has G$s to send in exchange
+    await gdframework.GoodDollar.mint(signer.address, ethers.constants.WeiPerEther);
+    const st = gdframework.GoodDollar;
+    const transferAction = await st.approve(pool.address, 1000);
+    const supportAction = await pool.support(signer.address, 1000, '0x');
+    expect(await gdframework.GoodDollar.balanceOf(pool.address)).eq(1000);
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution).equal(1000);
+    expect(supporter.lastUpdated).eq(0); //no update on single donation
+    expect(supporter.flowRate).equal(0);
+    expect(supportAction).to.emit(pool, 'SupporterUpdated').withArgs(signer.address, 0, 1000, 0, 0, false);
+  });
+
+  it('should be able to swap mockToken and stream when 0 G$ balance in one batch tx', async () => {
+    const signer = signers[1];
+
+    const mockToken = await (await ethers.getContractFactoryFromArtifact(ERC20ABI)).deploy('x', 'x');
+    await mockToken.mint(signer.address, ethers.constants.WeiPerEther);
+    await (await mockToken.connect(signer).approve(pool.address, ethers.constants.WeiPerEther)).wait();
+
+    expect(await gdframework.GoodDollar.balanceOf(signer.address)).equal(0);
+    expect(await mockToken.balanceOf(signer.address)).gt(0);
+
+    //mint to the swaprouter so it has G$s to send in exchange
+    await gdframework.GoodDollar.mint(await pool.swapRouter(), ethers.constants.WeiPerEther);
+
+    const st = await sf.loadSuperToken(gdframework.GoodDollar.address);
+    const appAction = pool.interface.encodeFunctionData('handleSwap', [
+      {
+        swapFrom: mockToken.address,
+        amount: ethers.constants.WeiPerEther,
+        minReturn: ethers.constants.WeiPerEther,
+        deadline: (Date.now() / 1000).toFixed(0),
+        path: '0x',
+      },
+      signer.address,
+      '0x',
+    ]);
+
+    const flowAction = st.createFlow({
+      receiver: pool.address,
+      sender: signer.address,
+      flowRate: baseFlowRate,
+    });
+
+    const bc = sf.batchCall([sf.host.callAppAction(pool.address, appAction), flowAction]);
+    const result = await bc.exec(signer);
+
+    expect(await mockToken.balanceOf(signer.address)).eq(0);
+    expect(await gdframework.GoodDollar.balanceOf(signer.address)).gt(0);
+
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution).equal(0);
+    expect(supporter.lastUpdated).gt(0);
+    expect(supporter.flowRate).equal(Number(baseFlowRate));
+  });
+
+  it('should be able to swap mockToken and support single when 0 G$ balance in one tx + approve', async () => {
+    const signer = signers[1];
+
+    const mockToken = await (await ethers.getContractFactoryFromArtifact(ERC20ABI)).deploy('x', 'x');
+    await mockToken.mint(signer.address, ethers.constants.WeiPerEther);
+    await (await mockToken.connect(signer).approve(pool.address, ethers.constants.WeiPerEther)).wait();
+
+    expect(await gdframework.GoodDollar.balanceOf(signer.address)).equal(0);
+    expect(await mockToken.balanceOf(signer.address)).gt(0);
+
+    //mint to the swaprouter so it has G$s to send in exchange
+    await gdframework.GoodDollar.mint(await pool.swapRouter(), ethers.constants.WeiPerEther);
+
+    const st = await sf.loadSuperToken(gdframework.GoodDollar.address);
+    const tx = await pool.connect(signer).supportWithSwap(signer.address, {
+      swapFrom: mockToken.address,
+      amount: ethers.constants.WeiPerEther,
+      minReturn: ethers.constants.WeiPerEther,
+      deadline: (Date.now() / 1000).toFixed(0),
+      path: '0x',
+    }, '0x')
+
+    // console.log((await tx.wait()).events)
+
+    expect(await mockToken.balanceOf(signer.address)).eq(0);
+    expect(await gdframework.GoodDollar.balanceOf(signer.address)).eq(0);
+
+    const supporter = await pool.supporters(signer.address);
+    expect(supporter.contribution).equal(ethers.constants.WeiPerEther);
+    expect(supporter.lastUpdated).eq(0);
+    expect(supporter.flowRate).equal(0);
+  });
+});
